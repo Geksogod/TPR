de1f9cf038bdc352fd1138998d0c8709b5845223
evgeniy.siecho <evgeniy.siecho@logika.com.ua>
2019-10-03 19:59:58 +0300

    Add NavMeshAgent

---
+++ Assets/Script/Impotant/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    bool check = false;
+    public void GoTo()
+    {
+        check = !check;
+    }
+
+    private void Update()
+    {
+        if (check)
+        {
+            GameObject target = this.gameObject.GetComponent<TargetFinder>().target;
+            this.gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+            this.gameObject.GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+        }
+    }
+}
