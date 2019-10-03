bbdc1ea41e39a9ae6037b41a8d4b3aa6b458ff11
evgeniy.siecho <evgeniy.siecho@logika.com.ua>
2019-10-03 22:01:05 +0300

    Update MovingSystem

--- Assets/Script/Good.cs
+++ Assets/Script/Good.cs
@@ -1,20 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Good : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private static GameObject target;
+    private bool check;
 
     public void AllGoToSleep()
     {
@@ -23,4 +15,54 @@ public class Good : MonoBehaviour
             GameObject.FindObjectsOfType<Animal>()[i].Energy = -100;
         }
     }
+
+    public void AllAnimalsFindTarget()
+    {
+        float distance = float.MaxValue;
+        target = GameObject.FindGameObjectWithTag("EdibleFlora");
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("EdibleFlora").Length; i++)
+        {
+            GameObject preTarget = GameObject.FindGameObjectsWithTag("EdibleFlora")[i];
+            float newDistanceF = Vector3.Distance(GameObject.FindGameObjectsWithTag("Animal")[0].transform.position, preTarget.transform.position);
+            if (newDistanceF < distance)
+            {
+                distance = newDistanceF;
+                target = preTarget;
+            }
+        }
+    }
+
+    #region TestCommand
+    public void AllAnimalsGoToTarget()
+    {
+        if (target != null)
+        {
+            check = true;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (check)
+        {
+            for (int i = 0; i < GameObject.FindGameObjectsWithTag("Animal").Length; i++)
+            {
+                //GameObject target = this.gameObject.GetComponent<TargetFinder>().target;
+                NavMeshAgent agent = GameObject.FindGameObjectsWithTag("Animal")[i].GetComponent<NavMeshAgent>();
+                agent.isStopped = false;
+                agent.destination = target.transform.position;
+                if (agent.hasPath == false)
+                {
+                    //agent.ResetPath();
+                    agent.destination = target.transform.position;
+                }
+                Debug.Log(agent.isStopped);
+                GameObject.FindGameObjectsWithTag("Animal")[i].transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+                //Debug.Log(GameObject.FindGameObjectsWithTag("Animal")[i].GetComponent<NavMeshAgent>().hasPath);
+            }
+        }
+    }
+    #endregion
 }
+
--- Assets/Script/Impotant/Movement.cs
+++ Assets/Script/Impotant/Movement.cs
@@ -5,19 +5,55 @@ using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
-    bool check = false;
+    private NavMeshAgent agent;
+    private bool check = false;
+    private bool MovingStart = false;
+    public bool drawPath;
+
+    private void Start()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
     public void GoTo()
     {
         check = !check;
+        MovingStart = true;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (check)
         {
             GameObject target = this.gameObject.GetComponent<TargetFinder>().target;
-            this.gameObject.GetComponent<NavMeshAgent>().isStopped = false;
-            this.gameObject.GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+            agent.destination = target.transform.position;
+            transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+            if (MovingStart && agent.hasPath&& !agent.pathPending)
+            {
+                DrawPath();
+                MovingStart = false;
+            }
         }
+    }
+
+    [System.Obsolete]
+    private void DrawPath()
+    {
+        if (agent == null || agent.path == null)
+            return;
+
+        LineRenderer line = this.GetComponent<LineRenderer>();
+        if (line == null)
+        {
+            line = this.gameObject.AddComponent<LineRenderer>();
+            line.material = new Material(Shader.Find("Sprites/Default")) { color = Color.yellow };
+            line.SetWidth(0.5f, 0.5f);
+            line.SetColors(Color.yellow, Color.yellow);
+
+        }
+
+        NavMeshPath path = agent.path;
+        line.SetVertexCount(path.corners.Length);
+        line.SetPositions(path.corners);
     }
 }
